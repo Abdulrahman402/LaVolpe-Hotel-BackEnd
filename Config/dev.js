258441f397d84c9901fda603a378cbd59d8564e4
Abdulrahman Mohammed <Abdulrahman402>
2020-07-02 07:29:55 +0200

    Config files added, Connected DB

---
+++ Config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://Abdu:123456789A@ds012188.mlab.com:12188/hotel-db",
+  tokenSecretKey: "myKey",
+};
--- index.js
+++ index.js
@@ -2,13 +2,21 @@ require("express-async-errors");
 const express = require("express");
 const winston = require("winston");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const keys = require("./Config/keys");
 
 app.get("/", async (req, res) => {
   res.send("Hello");
   console.log("Hi");
 });
+
+mongoose
+  .connect(keys.mongoURI)
+  .then(() => console.log("Connected to Hotel DB"))
+  .catch((err) => console.log("Error while connecting DB", err));
 
 const port = process.env.PORT || 2000;
 
