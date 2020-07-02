ec0b8aef79e4c95fc9a0a9a6851cafeb6a7560c4
Abdulrahman Mohammed <Abdulrahman402>
2020-07-02 11:21:54 +0200

    Added user model

---
+++ Models/User.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+const joi = require("joi");
+const jwt = require("jsonwebtoken");
+
+const keys = require("../Config/keys");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    oldBooking: {
+      roomId: {
+        type: Schema.Types.ObjectId,
+        ref: "Room",
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.generateAuthToken = async function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    keys.tokenSecretKey
+  );
+
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+  const schema = {
+    firstName: joi.string().required(),
+    lastName: joi.string().required(),
+    email: joi.string().required().email,
+    password: joi.string().required(),
+    phone: joi.number().required(),
+  };
+  return joi.validate(user, schema);
+}
+
+function updateUser(user) {
+  const schema = {
+    newFirstName: joi.string().required(),
+    newLastName: joi.string().required(),
+    oldPW: joi.string().required(),
+    newPW: joi.string().min(6).max(12).required(),
+    phone: joi.number().required(),
+  };
+  return joi.validate(user, schema);
+}
+
+module.exports = {
+  User,
+  validateUser,
+  updateUser,
+  userSchema,
+};
